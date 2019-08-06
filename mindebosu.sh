4aa3c63a10a3d4a1cb28e90ea61ab8e7b8529607
Kuba <kuba0303@onet.pl>
2019-08-06 23:58:47 +0200

    Mention of contributors

--- mindebosu.sh
+++ mindebosu.sh
@@ -6,4 +6,4 @@ winetricks gdiplus  # Required to render icons in settings and song selection.
 touch /home/"$USER"/.wine/drive_c/users/"$USER"/Local\ Settings/Application\ Data/osu\!/discord-rpc.dll  # Make a placeholder discord-rpc.dll, so the osu! installer won't get stuck.
 wget https://m1.ppy.sh/r/osu\!install.exe  # Download the latest osu! installer.
 wine osu\!install.exe  # Install osu! with wine.
-echo -e "\e[42mDONE\e[49m: osu! installed, for more info/updates/releases or bug reports, visit: https://github.com/dzejqub/mindebosu \nEnjoy playing osu on Linux! \n\n~dzejqub"
+echo -e "\e[42mDONE\e[49m: osu! installed, for more info/updates/releases or bug reports, visit: https://github.com/dzejqub/mindebosu \nEnjoy playing osu on Linux! \n\n~dzejqub, and mindebosu contributors"
